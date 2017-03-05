ae1ebb4d67d0771522c19b7ff476d45c6f4271b2
marks <woshishegu6275>
2017-03-05 21:28:59 +0800

    2017.3.5

--- app/src/main/java/com/example/marks/intern/CustomView.java
+++ app/src/main/java/com/example/marks/intern/CustomView.java
@@ -14,20 +14,21 @@ import android.view.View;
 
 public class CustomView extends View {
 
-    Paint paint;
-    Path path = new Path();
-    private int polygon,a,r,g,b;
-    private float x_start,x_end,y_start,y_end;
-    private float radius = 90-360/polygon;
-    private float length=getWidth()/polygon;
-    private float move_right= (float) (Math.cos(radius*Math.PI/180)*length);
-    private float move_above= (float) (Math.sin(radius*Math.PI/180)*length);
+
+    private int polygon=4;
+    private int a,r,g,b;
+    private float x_start,x_end,y;
+    private float radius = 360/polygon;
+
+
 
     public CustomView(Context context) {
+
         this(context,null);
     }
 
     public CustomView(Context context, AttributeSet attrs) {
+
         this(context, attrs,0);
     }
 
@@ -39,16 +40,16 @@ public class CustomView extends View {
             int attr=typedArray.getIndex(i);
             switch (attr){
                 case R.styleable.CustomView_customA:
-                    a=typedArray.getInteger(attr,255);
+                    a=typedArray.getInteger(attr,120);
                     break;
                 case R.styleable.CustomView_customB:
-                    b=typedArray.getInteger(attr,255);
+                    b=typedArray.getInteger(attr,120);
                     break;
                 case R.styleable.CustomView_customG:
-                    g=typedArray.getInteger(attr,255);
+                    g=typedArray.getInteger(attr,120);
                     break;
                 case R.styleable.CustomView_customR:
-                    r=typedArray.getInteger(attr,255);
+                    r=typedArray.getInteger(attr,120);
                     break;
                 case R.styleable.CustomView_polygon:
                     polygon=typedArray.getInteger(attr,4);
@@ -62,46 +63,73 @@ public class CustomView extends View {
 
     public void setPolygon(int polygon){
         this.polygon=polygon;
+        this.radius=360/polygon;
+        requestLayout();
+        invalidate();
 
     }
 
     public void setA(int a) {
+
         this.a = a;
+        requestLayout();
+        invalidate();
     }
 
     public void setR(int r) {
+
         this.r = r;
+        requestLayout();
+        invalidate();
     }
 
     public void setB(int b) {
+
         this.b = b;
+        requestLayout();
+        invalidate();
     }
 
     public void setG(int g) {
+
         this.g = g;
+        requestLayout();
+        invalidate();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
     }
 
     protected void onDraw(Canvas canvas) {
-        paint.setARGB(a,r,g,b);
-        paint.setStyle(Paint.Style.FILL);
-        x_start=0;
-        y_start=getHeight()/2;
-        path.moveTo(x_start, y_start);
+        super.onDraw(canvas);
+        float length= (float) (getWidth()/polygon);
+        if (!isInEditMode()){
+            canvas.translate(getWidth()/2,getHeight()/2);
+            Paint paint=new Paint();
+            Path path = new Path();
+            paint.setARGB(a,r,g,b);
+            paint.setStyle(Paint.Style.FILL);
+            x_start=-length/2;
+            x_end=length/2;
+            y= (float) ((length/2)/Math.tan(radius/2*Math.PI/180));
 
-        for (int i=1;i<=1;i++){
-            x_end=x_start+move_right;
-            y_end=y_start+move_above;
-            path.lineTo(x_end,y_end);
-            x_start=x_end;
-            y_start=y_end;
+            for (int i=1;i<=polygon;i++){
+
+                path.lineTo(x_start, y);
+                path.lineTo(x_end,y);
+                path.close();
+                canvas.drawPath(path,paint);
+                canvas.rotate(radius);
+
+
+            }
+
+
         }
-        path.close();
-        canvas.drawPath(path,paint);
+
 
     }
 }
--- app/src/main/java/com/example/marks/intern/MainActivity.java
+++ app/src/main/java/com/example/marks/intern/MainActivity.java
@@ -4,20 +4,60 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
     CustomView customView;
     EditText editText;
+    SeekBar seekBar1,seekBar2,seekBar3,seekBar4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         customView= (CustomView) findViewById(R.id.CustomView);
         editText= (EditText) findViewById(R.id.editText);
+        seekBar1 = (SeekBar) findViewById(R.id.seekBar1);
+        seekBar2= (SeekBar) findViewById(R.id.seekBar2);
+        seekBar3 = (SeekBar) findViewById(R.id.seekBar3);
+        seekBar4 = (SeekBar) findViewById(R.id.seekBar4);
+
+        seekBar1.setOnSeekBarChangeListener(this);
+        seekBar2.setOnSeekBarChangeListener(this);
+        seekBar3.setOnSeekBarChangeListener(this);
+        seekBar4.setOnSeekBarChangeListener(this);
     }
     public void onClick(View view){
         int polygon= Integer.parseInt(editText.getText().toString());
         customView.setPolygon(polygon);
-        customView.invalidate();
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId()){
+            case R.id.seekBar1:
+                customView.setA(progress);
+                break;
+            case R.id.seekBar2:
+                customView.setR(progress);
+                break;
+            case R.id.seekBar3:
+                customView.setG(progress);
+                break;
+            case R.id.seekBar4:
+                customView.setB(progress);
+                break;
+
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
